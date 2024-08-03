e3b1057d9396f410fc7d4da079b1d92088104c9f
ajaberi <adnan.ajaberi@gmail.com>
2024-08-03 20:15:53 +0300

    added leaderboard

--- app/Http/Controllers/LeaderboardController.php
+++ app/Http/Controllers/LeaderboardController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class LeaderboardController extends Controller
 {
     public function index()
     {
-        return view('leaderboard');
+        $TopTenUsers = User::orderBy('xp', 'desc')
+            ->take(10)
+            ->get();
+        return view('leaderboard', [
+            'TopTenUsers' => $TopTenUsers
+        ]);
     }
 }
--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -1,9 +1,45 @@
 @extends('app')
 
 @section('content')
+    <a class="top-right-corner red-btn" href="{{ route('home') }}">Back ></a>
 
-<table>
-
-</table>
-
+    <div style="margin-top:100px; display: flex; justify-content: center;">
+        <table style="border-collapse: collapse; width: 80%; text-align: center;">
+            <thead>
+                <tr>
+                    <th style="border: 1px solid blue; padding: 8px; color:aqua">Rank</th>
+                    <th style="border: 1px solid blue; padding: 8px; color:aqua">Username</th>
+                    <th style="border: 1px solid blue; padding: 8px; color:aqua">XP</th>
+                    <th style="border: 1px solid blue; padding: 8px; color:aqua">Rank Title</th>
+                </tr>
+            </thead>
+            <tbody style="color: white">
+                @foreach ($TopTenUsers as $index => $user)
+                    @php
+                        $userxp = $user->xp;
+                        $geographyscore = $user->geography;
+                        $historyscore = $user->history;
+                        $sciencescore = $user->science;
+                        $sportscore = $user->sports;
+                    @endphp
+                    <tr>
+                        <td style="border: 1px solid blue; padding: 8px;">{{ $index + 1 }}</td>
+                        <td style="border: 1px solid blue; padding: 8px;">{{ $user->username }}</td>
+                        <td style="border: 1px solid blue; padding: 8px;">{{ $userxp }} XP</td>
+                        <td style="border: 1px solid blue; padding: 8px;">
+                            @if ($userxp < 1500)
+                                Quiz Apprentice
+                            @elseif($userxp < 5000)
+                                Average Quizer
+                            @elseif($userxp < 10000)
+                                Epic Quizer
+                            @else
+                                Quiz Master
+                            @endif
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
