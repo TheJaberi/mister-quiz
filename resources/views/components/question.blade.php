ceec5ea4c6d2c8b4a4c96a626ff4a39cff2b8f37
sayhusain <hishamalmosawii@gmail.com>
2024-08-12 13:52:30 +0300

    adding category and xp to each question

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -1,7 +1,8 @@
 @props(['question' => $question])
 
 <div class="mb4">
-    <p class="center title">{{ $question->question }}</p>
+    <p class="center title"> Category: {{ $question->category }} - {{ $question->xp }}XP</p>
+    <p class="center title">{{ $question->question }} </p>
 
     <div class="checkboxes-wrapper" class="center">
         @foreach ($question->answers as $answer)
